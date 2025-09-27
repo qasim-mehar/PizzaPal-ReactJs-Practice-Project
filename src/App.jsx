aa554dd23b808e6d29fc1652c5f06e520374e123
Qasim <mianqasim065@gmail.com>
2025-09-27 17:47:14 +0500

    Add cart compoenent

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import React from "react";
 
 import Home from "./ui/Home"
 import Menu from "./features/menu/Menu"
-import Cart from "./features/cart/CartOverview";
+// import Cart from "./features/cart/CartOverview";
 import CreateOrder from "./features/order/CreateOrder"
 import Order from "./features/order/Order"
 import AppLayout from "./ui/AppLayout";
+import Cart from "./features/cart/Cart";
 
 const router=createBrowserRouter([
   //WE USE THIS PERTICULAR IMPERITIVE WAY TO DECLARE ROUTES ONLY BECAUSE HERE WE CAN LOAD DATA UNLICE OLD BROWSERROUTE> ROUTES WAY.
