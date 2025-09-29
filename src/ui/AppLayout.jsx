3eb8b7a53ee18f8a883f27d0905874c952516081
Qasim <mianqasim065@gmail.com>
2025-09-29 23:24:34 +0500

    Feat: Implement loader while fetching data using useNavigation

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import React from "react";
 import CartOverview from "../features/cart/CartOverview"
+import Loader from "./Loader";
 function AppLayout() {
+  const navigation= useNavigation();
+  const isLoadding=navigation.state==="loading"
   return (
-    <div>
-
+    <div className="layout">
+     {isLoadding&&<Loader/>}
       <Header/>
       <main>
 
