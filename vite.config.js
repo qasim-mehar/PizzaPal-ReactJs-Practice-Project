65f3bcc02b4dadf27c9b7401d35a38eed7015e57
Qasim <mianqasim065@gmail.com>
2025-10-02 21:55:05 +0500

    Setup tailwind css

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     eslint({
       cache: false, // always lint freshly (optional)
       include: ["src/**/*.js", "src/**/*.jsx"],
